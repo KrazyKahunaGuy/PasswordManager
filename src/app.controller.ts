0c8f5a0042f1eb299a14bcf96a8f16284a7ef16d
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2023-04-21 11:37:26 +0100

    Modify getHello endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test-request')
   getHello(): string {
     return this.appService.getHello();
   }
