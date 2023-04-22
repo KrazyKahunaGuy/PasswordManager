14d8e8829b50d158bf9d418b54aa93f72e2d443e
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2023-04-22 11:34:44 +0100

    Add ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PasswordModule } from './password/password.module';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [UserModule, PasswordModule,],
+  imports: [
+    UserModule,
+    PasswordModule,
+    AuthModule,
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env`})
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
